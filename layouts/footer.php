60b643afdcaf7bc60f559da4a110edbaac61502e
DESKTOP-T64OH4K\jiemu <jiemukun@gmail.com>
2024-12-24 01:35:06 +0800

    hotfix - remove local timeout

--- layouts/footer.php
+++ layouts/footer.php
@@ -3,7 +3,7 @@
     <p class="text-center">I'm your footer <br> ©John Michael Velasco</p>
 </footer>
 
-<script>
+<!-- <script>
       
     var timeoutDuration =  10 * 1000; 
     var logoutTimer;
@@ -21,7 +21,7 @@
     document.onmousemove = resetTimer;
     document.onkeypress = resetTimer;
 
-</script>
+</script> -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
